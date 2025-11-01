b6e79674bc2ba031924879f50820e79db281f4b1
Anand Mishra <anandmishra@Anands-MacBook-Air-2.local>
2025-11-01 15:11:13 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+
+interface Tree {
+  [key: string]: string[];
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Tree-structure');
-}
+  readonly input: Tree = {
+    a: ['b', 'c'],
+    b: ['d', 'e'],
+    c: ['f', 'g'],
+    e: ['h', 'i'],
+    f: ['j', 'k'],
+  };
+
+  public getRoot() {
+    const allChild = new Set<string>();
+
+    Object.values(this.input).forEach(children => {
+      children.forEach(child => allChild.add(child));
+    })
+
+    const root = Object.keys(this.input).find(key => !allChild.has(key));
+    console.log(allChild, root);
+
+    return root;
+  }
+ }
